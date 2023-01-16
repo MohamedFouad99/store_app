d56aa6a543c80f9b8e53fa90cf1fd684c366e4c1
Mohamed Fouad <m.elsayedfouad99@gmail.com>
2023-01-16 18:26:57 +0200

    edit_product

--- lib/services/update_product.dart
+++ lib/services/update_product.dart
@@ -1,15 +1,16 @@
 import '../helper/api.dart';
 import '../models/product_model.dart';
 
-class UpdateProduct {
+class UpdateProductService {
   Future<ProductModel> updateProduct(
       {required String title,
       required String price,
       required String description,
       required String image,
+      required int id,
       required String category}) async {
     Map<String, dynamic> data = await Api().put(
-      url: 'https://fakestoreapi.com/products',
+      url: 'https://fakestoreapi.com/products/$id',
       body: {
         'title': title,
         'price': price,
